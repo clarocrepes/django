dcb4dafaf41ef7388ae232fb088e7f947bdf2e43
kali <kali@kali.onion>
2022-04-02 13:01:52 -0400

    added payment details

--- app/checkout/views.py
+++ app/checkout/views.py
@@ -2,5 +2,9 @@ from oscar.apps.checkout.views import *
 
 class PaymentDetailsView(PaymentDetailsView):
     template_name = 'payment_details.html'
+    preview = False
+
+class ThankYouView(ThankYouView):
+    template_name = 'thank_you.html'
 
 
