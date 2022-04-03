62991ca4137c10374f243746bba078f1ec85c77c
kali <kali@kali.onion>
2022-04-02 21:29:00 -0400

    finished devops

--- app/checkout/views.py
+++ app/checkout/views.py
@@ -1,10 +1,10 @@
 from oscar.apps.checkout.views import *
 
+
 class PaymentDetailsView(PaymentDetailsView):
     template_name = 'payment_details.html'
     preview = False
 
+
 class ThankYouView(ThankYouView):
     template_name = 'thank_you.html'
-
-
